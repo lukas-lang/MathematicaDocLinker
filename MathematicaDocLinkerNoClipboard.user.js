af45d127c8d16e852715bb422cebbfbcdbc64740
Mathe172 <mathe172@outlook.com>
2019-12-01 16:41:48 +0100

    Fixed script not being applied on guide/... pages

--- MathematicaDocLinkerNoClipboard.user.js
+++ MathematicaDocLinkerNoClipboard.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Mathematica Doc Linker
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @downloadURL  https://github.com/lukas-lang/MathematicaDocLinker/raw/master/MathematicaDocLinker.user.js
 // @description  Enables the creation of links to specific parts of the Mathematica Web Documentation
 // @author       Lukas Lang
-// @match        https://reference.wolfram.com/language/ref/*
+// @match        https://reference.wolfram.com/language/*
 // @grant        none
 // ==/UserScript==
 
